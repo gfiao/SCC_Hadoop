2ea050e0d42e5f4d6dafff999615091941b38134
gfiao <guilherme.fiao@gmail.com>
2014-11-20 14:51:41 +0000

    lib a usar para fazer parse

---
+++ src/tp2/Tp2.java
@@ -0,0 +1,13 @@
+package tp2;
+
+import org.apache.hadoop.*;
+
+public class Tp2 {
+
+    public static void main(String[] args) throws Exception {
+
+    	
+    	
+    }
+
+}
--- src/tp2/tp2.java
+++
@@ -1,9 +0,0 @@
-package tp2;
-
-public class tp2 {
-
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
