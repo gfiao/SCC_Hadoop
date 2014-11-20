22b02f362b1af19abd7dcd2c69ab92de781db831
gfiao <guilherme.fiao@gmail.com>
2014-11-20 16:24:08 +0000

    exemple map-reduce

--- src/warc_mapreduce/WarcFileRecordReader.java
+++ src/warc_mapreduce/WarcFileRecordReader.java
@@ -36,13 +36,9 @@ package warc_mapreduce;
 
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.URI;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.compress.CompressionCodec;
